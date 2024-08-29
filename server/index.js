eaf19bbcdafea040fac108171e9b3bfa53cd0e61
tranvietanh20112001 <tranvietanh20112001@gmail.com>
2024-08-29 08:57:13 +0700

    update

---
+++ server/index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
+const cors = require("cors");
+const path = require("path");
+
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URL);
+
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.log(error.message);
+    process.exit(1);
+  }
+};
+
+connectDB();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
+
+app.use(cors({ origin: "*" }));
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
